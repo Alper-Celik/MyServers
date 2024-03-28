c0877a565a37208622c29c9209b729079831c47a
Alper Çelik <alper@alper-celik.dev>
2024-03-28 17:45:30 +0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    deploy-rs.url = "github:serokell/deploy-rs";
+  };
+
+  outputs = { self, nixpkgs, deploy-rs }: {
+
+    nixosConfigurations.rpi5 = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+
+      modules = [ ./configuration.nix ];
+    };
+
+    deploy.nodes.rpi5 = {
+
+      hostname = "192.168.1.103";
+      sshUser = "root";
+      remoteBuild = true;
+
+      profiles = {
+        system = {
+          user = "root";
+          path = deploy-rs.lib.aarch64-linux.activate.nixos
+            self.nixosConfigurations.rpi5;
+        };
+      };
+    };
+
+    # checks =
+    #   builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy)
+    #   deploy-rs.lib;
+  };
+}
