1899abfe019273d684af8f7bc3dc1db441c94bd2
Alper Çelik <alper@alper-celik.dev>
2024-03-31 10:45:07 +0300

    .

---
+++ minecraft-server.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+let
+  generic-server = pkgs.writeShellApplication {
+    name = "minecraft-server";
+
+    runtimeInputs = [ pkgs.jre8 ];
+
+    text = ''
+      exec java "$@" -jar /var/lib/minecraft/server.jar nogui
+    '';
+  };
+in
+{
+  services.minecraft-server = {
+    package = generic-server;
+    enable = true;
+    eula = true;
+    openFirewall = true;
+    jvmOpts = "-server -Xms2G -Xmx2G";
+  };
+}
+
