f42c84034d45eac2edec9d0d5938688c6932879d
Alper Çelik <alper@alper-celik.dev>
2024-06-02 17:48:10 +0300

    enable minecraft server with updated config

--- minecraft-server.nix
+++ minecraft-server.nix
@@ -3,20 +3,21 @@ let
   generic-server = pkgs.writeShellApplication {
     name = "minecraft-server";
 
-    runtimeInputs = [ pkgs.jre8 ];
+    runtimeInputs = [ pkgs.jdk17 ];
 
     text = ''
-      exec java "$@" -jar /var/lib/minecraft/server.jar nogui
+      exec /var/lib/minecraft/ServerStart.sh
     '';
   };
 in
 {
   services.minecraft-server = {
     package = generic-server;
-    # enable = true;
+    enable = true;
     eula = true;
     openFirewall = true;
-    jvmOpts = "-server -Xms2G -Xmx2G";
+    # jvmOpts = "-server -Xms2G -Xmx2G";
+    jvmOpts = "";
   };
 }
 
