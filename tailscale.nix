a82698a647bd8d61aa1a7b86836bfb82f4762989
Alper Çelik <alper@alper-celik.dev>
2024-10-07 01:44:37 +0300

    tailscale register exit node

--- tailscale.nix
+++ tailscale.nix
@@ -1,6 +1,8 @@
 { config, ... }:
 {
   services.tailscale = {
+    useRoutingFeatures = "server";
+    extraSetFlags = [ "--advertise-exit-node" ];
     enable = true;
     authKeyFile = config.sops.secrets.tailscale-rpi5.path;
   };
