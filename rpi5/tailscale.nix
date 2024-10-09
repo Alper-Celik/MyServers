6b03ad1462e9dafa9642c917c410a62383da12be
Alper Çelik <alper@alper-celik.dev>
2024-10-09 10:51:11 +0300

    fix tailscale problem

--- rpi5/tailscale.nix
+++ rpi5/tailscale.nix
@@ -3,6 +3,7 @@
   services.tailscale = {
     useRoutingFeatures = "server";
     extraSetFlags = [ "--advertise-exit-node" ];
+    extraUpFlags = [ "--advertise-exit-node" ];
     enable = true;
     authKeyFile = config.sops.secrets.tailscale-rpi5.path;
   };
