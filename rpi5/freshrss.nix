3c26cceead2d9b4955f4781ecd7acaf929e1d8bb
Alper Çelik <alper@alper-celik.dev>
2024-10-31 01:36:21 +0300

    switch to unstable since modules are in are in a better shape in lastest version of nixpkgs

--- rpi5/freshrss.nix
+++ rpi5/freshrss.nix
@@ -1,32 +1,32 @@
 { config, ... }:
 {
-  services.postgresql = {
-    enable = true;
-    ensureDatabases = [ config.services.freshrss.database.name ];
-    ensureUsers = [
-      {
-        name = config.services.freshrss.user;
-        ensureDBOwnership = true;
-      }
-    ];
-  };
-
-  services.freshrss = {
-    enable = true;
-    baseUrl = "https://${config.services.freshrss.virtualHost}";
-    virtualHost = "freshrss.lab.alper-celik.dev";
-    passwordFile = config.sops.secrets.nextcloud-admin-pass.path;
-    database = {
-      name = "freshrss";
-      user = "freshrss";
-      type = "pgsql";
-      host = "/var/postgresql";
-    };
-  };
-
-  services.nginx.virtualHosts.${config.services.freshrss.virtualHost} = {
-    forceSSL = true;
-    enableACME = true;
-    acmeRoot = null;
-  };
+  # services.postgresql = {
+  #   enable = true;
+  #   ensureDatabases = [ config.services.freshrss.database.name ];
+  #   ensureUsers = [
+  #     {
+  #       name = config.services.freshrss.user;
+  #       ensureDBOwnership = true;
+  #     }
+  #   ];
+  # };
+  #
+  # services.freshrss = {
+  #   enable = true;
+  #   baseUrl = "https://${config.services.freshrss.virtualHost}";
+  #   virtualHost = "freshrss.lab.alper-celik.dev";
+  #   passwordFile = config.sops.secrets.nextcloud-admin-pass.path;
+  #   database = {
+  #     name = "freshrss";
+  #     user = "freshrss";
+  #     type = "pgsql";
+  #     host = "/var/postgresql";
+  #   };
+  # };
+  #
+  # services.nginx.virtualHosts.${config.services.freshrss.virtualHost} = {
+  #   forceSSL = true;
+  #   enableACME = true;
+  #   acmeRoot = null;
+  # };
 }
