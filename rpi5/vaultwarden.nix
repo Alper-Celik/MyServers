86e2cf9ce72883255c0ef117263208e25a1b61cd
Alper Çelik <alper@alper-celik.dev>
2024-10-31 01:03:05 +0300

    switch vaultwarden to postgresql

--- rpi5/vaultwarden.nix
+++ rpi5/vaultwarden.nix
@@ -1,7 +1,42 @@
 { config, ... }:
+let
+  vaultwarden-db = "vaultwarden";
+  vaultwarden-db-user = "vaultwarden";
+in
 {
+
+  environment.persistence."/persistent-important" = {
+
+    enable = true;
+    directories = [
+      {
+        directory = "/var/lib/bitwarden_rs/";
+        user = config.users.users.vaultwarden.name;
+        group = config.users.users.vaultwarden.name;
+        mode = "u=rwx,g=,o=";
+      }
+
+    ];
+  };
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ vaultwarden-db ];
+    ensureUsers = [
+      {
+        name = vaultwarden-db-user;
+        ensureDBOwnership = true;
+      }
+    ];
+  };
   services.vaultwarden = {
     enable = true;
+    dbBackend = "postgresql";
+    config = {
+      ROCKET_ADDRESS = "::1";
+      ROCKET_PORT = 8222;
+
+      DATABASE_URL = "postgresql://%2Frun%2Fpostgresql/${vaultwarden-db}?user=${vaultwarden-db-user}";
+    };
   };
 
   services.nginx.virtualHosts."bitwarden.lab.alper-celik.dev" = {
