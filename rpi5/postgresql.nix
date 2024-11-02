1cc044ea484ad1b4da7c02d2369030323a087ad3
Alper Çelik <alper@alper-celik.dev>
2024-11-03 02:56:07 +0300

    add postgresql config

---
+++ rpi5/postgresql.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+{
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_15;
+    enableJIT = true;
+  };
+
+  services.pgadmin = {
+    enable = true;
+    initialEmail = "alper@alper-celik.dev";
+    initialPasswordFile = config.sops.secrets.pgadmin-pass.path;
+  };
+
+  services.nginx.virtualHosts."pgadmin.lab.alper-celik.dev" = {
+    enableACME = true;
+    forceSSL = true;
+    acmeRoot = null;
+    locations."/" = {
+      proxyPass = "http://localhost:${toString config.services.pgadmin.port}";
+    };
+  };
+
+}
