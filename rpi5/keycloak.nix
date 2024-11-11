624a7688ca29b5505b846c4e638b83f2b32ec530
Alper Çelik <alper@alper-celik.dev>
2024-11-11 22:44:55 +0300

    some more keycloak

--- rpi5/keycloak.nix
+++ rpi5/keycloak.nix
@@ -4,9 +4,19 @@
     forceSSL = true;
     enableACME = true;
     acmeRoot = null;
+    # expose = true;
     locations = {
       "/" = {
         proxyPass = "http://localhost:${toString config.services.keycloak.settings.http-port}";
+        extraConfig = ''
+          proxy_set_header    Host               $host;
+          proxy_set_header    X-Real-IP          $remote_addr;
+          proxy_set_header    X-Forwarded-For    $proxy_add_x_forwarded_for;
+          proxy_set_header    X-Forwarded-Host   $host;
+          proxy_set_header    X-Forwarded-Server $host;
+          proxy_set_header    X-Forwarded-Port   $server_port;
+          proxy_set_header    X-Forwarded-Proto  $scheme;
+        '';
       };
     };
   };
@@ -20,6 +30,7 @@
     };
     settings = {
       hostname = "id.lab.alper-celik.dev";
+      proxy-headers = "xforwarded";
       hostname-port = 443;
       http-port = 38080;
       http-enabled = true;
