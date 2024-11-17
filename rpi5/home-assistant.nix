8c877bc1e00ef72aed441d0d48da6f34e2cccb84
Alper Çelik <alper@alper-celik.dev>
2024-11-17 04:19:06 +0300

    add home assistant

---
+++ rpi5/home-assistant.nix
@@ -0,0 +1,32 @@
+{ ... }:
+{
+  virtualisation.oci-containers.containers."home-assistant" = {
+    image = "ghcr.io/home-assistant/home-assistant:stable";
+    volumes = [
+      "/var/lib/home-assistant:/config"
+      "/etc/localtime:/etc/localtime:ro"
+      "/run/postgresql/:/run/postgresql/"
+      "/run/dbus:/run/dbus:ro"
+    ];
+    environment = {
+      # DISABLE_JEMALLOC = "true"; # enable it if using bigger than 4kb page sizes
+    };
+    extraOptions = [
+      "--network=host"
+      "--privileged"
+    ];
+  };
+
+  services.nginx = {
+    virtualHosts."home.lab.alper-celik.dev" = {
+      expose = true;
+      forceSSL = true;
+      enableACME = true;
+      acmeRoot = null;
+      locations."/" = {
+        proxyPass = "http://[::1]:8123";
+        proxyWebsockets = true;
+      };
+    };
+  };
+}
