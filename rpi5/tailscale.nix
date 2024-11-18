62d0a3e066b8e4bc1f7efce5e125c620c4b7dd77
Alper Çelik <alper@alper-celik.dev>
2024-11-18 20:09:38 +0300

    advertise subnet

--- rpi5/tailscale.nix
+++ rpi5/tailscale.nix
@@ -1,9 +1,12 @@
 { config, ... }:
 {
   services.tailscale = {
-    useRoutingFeatures = "server";
+    useRoutingFeatures = "both";
     extraSetFlags = [ "--advertise-exit-node" ];
-    extraUpFlags = [ "--advertise-exit-node" ];
+    extraUpFlags = [
+      "--advertise-exit-node"
+      "--advertise-routes=192.168.1.0/24"
+    ];
     enable = true;
     authKeyFile = config.sops.secrets.tailscale-auth-key.path;
   };
