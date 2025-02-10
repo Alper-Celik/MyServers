5bb244c3de9921111cc6e9c85fccd65df93d0457
Alper Çelik <alper@alper-celik.dev>
2025-02-11 02:41:58 +0300

    dns creating problems o it isnt needed anyway

--- rpi5/dns.nix
+++ rpi5/dns.nix
@@ -11,48 +11,48 @@ let
 in
 {
 
-  systemd.services."octo-dns-update" = {
-    enable = true;
-    wantedBy = [ "network-online.target" ];
-    script = ''
-      set -eu
-      export CLOUDFLARE_TOKEN=$(cat ${config.sops.secrets.CLOUDFLARE_TOKEN-dns.path})
-      ${fpkgs.octodns}/bin/octodns-sync --config-file "${fpkgs.octodns-config}" --force --doit 
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-      User = config.users.users.octo-dns.name;
-    };
-    startAt = "*:0/5";
-
-  };
-
-  users.users.octo-dns = {
-    isSystemUser = true;
-    group = "octo-dns";
-  };
-  users.groups.octo-dns = { };
-
-  services.unbound = {
-    # FIXME: unbound doesnt support ttl noooooo
-
-    # enable = true;
-    settings = {
-      server = {
-        interface = [
-          "127.0.0.1"
-          "tailscale0"
-        ];
-        local-zone = "alper-celik.dev static";
-        local-data = builtins.map (record: "\"" + (lib.strings.escape [ "\"" ] record) + "\"") (
-          (lib.strings.splitString "\n" (builtins.readFile "${fpkgs.zoneFiles}/alper-celik.dev"))
-          ++ [
-            # "rpi5.devices.alper-celik.dev. IN 300 A 100.104.52.23"
-          ]
-        );
-      };
-    };
-  };
+  # systemd.services."octo-dns-update" = {
+  #   enable = true;
+  #   wantedBy = [ "network-online.target" ];
+  #   script = ''
+  #     set -eu
+  #     export CLOUDFLARE_TOKEN=$(cat ${config.sops.secrets.CLOUDFLARE_TOKEN-dns.path})
+  #     ${fpkgs.octodns}/bin/octodns-sync --config-file "${fpkgs.octodns-config}" --force --doit
+  #   '';
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     User = config.users.users.octo-dns.name;
+  #   };
+  #   startAt = "*:0/5";
+  #
+  # };
+  #
+  # users.users.octo-dns = {
+  #   isSystemUser = true;
+  #   group = "octo-dns";
+  # };
+  # users.groups.octo-dns = { };
+  #
+  # services.unbound = {
+  #   # FIXME: unbound doesnt support ttl noooooo
+  #
+  #   # enable = true;
+  #   settings = {
+  #     server = {
+  #       interface = [
+  #         "127.0.0.1"
+  #         "tailscale0"
+  #       ];
+  #       local-zone = "alper-celik.dev static";
+  #       local-data = builtins.map (record: "\"" + (lib.strings.escape [ "\"" ] record) + "\"") (
+  #         (lib.strings.splitString "\n" (builtins.readFile "${fpkgs.zoneFiles}/alper-celik.dev"))
+  #         ++ [
+  #           # "rpi5.devices.alper-celik.dev. IN 300 A 100.104.52.23"
+  #         ]
+  #       );
+  #     };
+  #   };
+  # };
 
   # services.bind = {
   #   enable = true;
