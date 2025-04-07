df0bef157ce70d4332dea022485a2aa2dfd0dce3
Alper Çelik <alper@alper-celik.dev>
2025-04-08 00:04:55 +0300

    disable keycloak for now since it is broken now

--- rpi5/keycloak.nix
+++ rpi5/keycloak.nix
@@ -1,40 +1,40 @@
 { config, ... }:
 {
-  services.nginx.virtualHosts."id.lab.alper-celik.dev" = {
-    forceSSL = true;
-    enableACME = true;
-    acmeRoot = null;
-    expose = true;
-    locations = {
-      "/" = {
-        proxyPass = "http://localhost:${toString config.services.keycloak.settings.http-port}";
-        extraConfig = ''
-          proxy_set_header    Host               $host;
-          proxy_set_header    X-Real-IP          $remote_addr;
-          proxy_set_header    X-Forwarded-For    $proxy_add_x_forwarded_for;
-          proxy_set_header    X-Forwarded-Host   $host;
-          proxy_set_header    X-Forwarded-Server $host;
-          proxy_set_header    X-Forwarded-Port   $server_port;
-          proxy_set_header    X-Forwarded-Proto  $scheme;
-        '';
-      };
-    };
-  };
-
-  services.keycloak = {
-    enable = true;
-    database = {
-      type = "postgresql";
-      passwordFile = config.sops.secrets."postgres/keycloak-pass".path;
-      createLocally = true;
-    };
-    settings = {
-      hostname = "id.lab.alper-celik.dev";
-      proxy-headers = "xforwarded";
-      hostname-port = 443;
-      http-port = 38080;
-      http-enabled = true;
-    };
-
-  };
+  # services.nginx.virtualHosts."id.lab.alper-celik.dev" = {
+  #   forceSSL = true;
+  #   enableACME = true;
+  #   acmeRoot = null;
+  #   expose = true;
+  #   locations = {
+  #     "/" = {
+  #       proxyPass = "http://localhost:${toString config.services.keycloak.settings.http-port}";
+  #       extraConfig = ''
+  #         proxy_set_header    Host               $host;
+  #         proxy_set_header    X-Real-IP          $remote_addr;
+  #         proxy_set_header    X-Forwarded-For    $proxy_add_x_forwarded_for;
+  #         proxy_set_header    X-Forwarded-Host   $host;
+  #         proxy_set_header    X-Forwarded-Server $host;
+  #         proxy_set_header    X-Forwarded-Port   $server_port;
+  #         proxy_set_header    X-Forwarded-Proto  $scheme;
+  #       '';
+  #     };
+  #   };
+  # };
+  #
+  # services.keycloak = {
+  #   enable = true;
+  #   database = {
+  #     type = "postgresql";
+  #     passwordFile = config.sops.secrets."postgres/keycloak-pass".path;
+  #     createLocally = true;
+  #   };
+  #   settings = {
+  #     hostname = "id.lab.alper-celik.dev";
+  #     proxy-headers = "xforwarded";
+  #     hostname-port = 443;
+  #     http-port = 38080;
+  #     http-enabled = true;
+  #   };
+  #
+  # };
 }
