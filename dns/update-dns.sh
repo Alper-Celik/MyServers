070a4ac9b606c579f58c15311be736b20cfd322a
Alper Çelik <alper@alper-celik.dev>
2025-04-25 18:28:15 +0300

    disable dns update for now

--- dns/update-dns.sh
+++ dns/update-dns.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env nix
 #! nix shell ../#octodns --command bash
 cd "$(dirname "$(readlink -f "$0")")"
-octodns-sync --config-file ./production.yaml "$@"
+# TODO: Fix actions
+# octodns-sync --config-file ./production.yaml "$@"
