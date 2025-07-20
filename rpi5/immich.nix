a438e2353cbb93c1da6c9cccb7122e1adb581b12
Alper Çelik <alper@alper-celik.dev>
2025-07-20 23:30:34 +0300

    enable immich

--- rpi5/immich.nix
+++ rpi5/immich.nix
@@ -1,37 +1,42 @@
 { config, ... }:
 {
-  # users.users.immich.extraGroups =
-  #   [ "video" "render" config.users.groups.media.name ];
-  # services.immich = {
-  #   enable = true;
-  #   environment = { IMMICH_TELEMETRY_INCLUDE = "all"; };
-  # };
-  #
-  # services.nginx.virtualHosts."photos.lab.alper-celik.dev" = {
-  #   forceSSL = true;
-  #   enableACME = true;
-  #   acmeRoot = null;
-  #
-  #   expose = true;
-  #
-  #   locations."/" = {
-  #     proxyWebsockets = true;
-  #     proxyPass = "http://[::1]:${toString config.services.immich.port}";
-  #     extraConfig = ''
-  #       # allow large file uploads
-  #       client_max_body_size 50000M;
-  #
-  #       # Set headers
-  #       proxy_set_header Host              $host;
-  #       proxy_set_header X-Real-IP         $remote_addr;
-  #       proxy_set_header X-Forwarded-For   $proxy_add_x_forwarded_for;
-  #       proxy_set_header X-Forwarded-Proto $scheme;
-  #
-  #       # set timeout
-  #       proxy_read_timeout 600s;
-  #       proxy_send_timeout 600s;
-  #       send_timeout       600s;
-  #     '';
-  #   };
-  # };
+  users.users.immich.extraGroups = [
+    "video"
+    "render"
+    config.users.groups.media.name
+  ];
+  services.immich = {
+    enable = true;
+    environment = {
+      IMMICH_TELEMETRY_INCLUDE = "all";
+    };
+  };
+
+  services.nginx.virtualHosts."photos.lab.alper-celik.dev" = {
+    forceSSL = true;
+    enableACME = true;
+    acmeRoot = null;
+
+    expose = true;
+
+    locations."/" = {
+      proxyWebsockets = true;
+      proxyPass = "http://[::1]:${toString config.services.immich.port}";
+      extraConfig = ''
+        # allow large file uploads
+        client_max_body_size 50000M;
+
+        # Set headers
+        proxy_set_header Host              $host;
+        proxy_set_header X-Real-IP         $remote_addr;
+        proxy_set_header X-Forwarded-For   $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+
+        # set timeout
+        proxy_read_timeout 600s;
+        proxy_send_timeout 600s;
+        send_timeout       600s;
+      '';
+    };
+  };
 }
