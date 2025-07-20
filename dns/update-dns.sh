0e5f7cdd20fced0e358c74d095d0449138560692
Alper Çelik <alper@alper-celik.dev>
2025-07-20 23:30:52 +0300

    enable dns auto update

--- dns/update-dns.sh
+++ dns/update-dns.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env nix
 #! nix shell ../#octodns --command bash
 cd "$(dirname "$(readlink -f "$0")")"
-# TODO: Fix actions
-# octodns-sync --config-file ./production.yaml "$@"
+octodns-sync --config-file ./production.yaml "$@"
