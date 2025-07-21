a082453627339e04f552fd76b38c4b718d29206e
Alper Çelik <alper@alper-celik.dev>
2025-07-21 22:36:26 +0300

    fix nginx warning
    warning : "could not build optimal proxy_headers_hash, you should increase either proxy_headers_hash_max_size: 512 or proxy_headers_hash_bucket_size: 64; ignoring proxy_headers_hash_bucket_size"

--- rpi5/webserver.nix
+++ rpi5/webserver.nix
@@ -37,6 +37,11 @@ in
       recommendedProxySettings = true;
       recommendedOptimisation = true;
 
+      appendHttpConfig = ''
+        proxy_headers_hash_bucket_size 128;
+        proxy_headers_hash_max_size 1024;
+      '';
+
       virtualHosts."_".locations."/".extraConfig = ''
         allow 100.64.0.0/10;
 
