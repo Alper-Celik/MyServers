a64abf8b9b9fc6b2efa6c9c17545b54424a90b1e
Alper Çelik <alper@alper-celik.dev>
2025-07-23 13:29:42 +0300

    add navidrome

---
+++ rpi5/music.nix
@@ -0,0 +1,26 @@
+{ config, ... }:
+{
+  services.navidrome = {
+    enable = true;
+    settings = {
+      MusicFolder = "/var/lib/nextcloud/data/alper/files/Music";
+    };
+  };
+
+  services.nginx.virtualHosts."music.lab.alper-celik.dev" = {
+    forceSSL = true;
+    enableACME = true;
+    acmeRoot = null;
+
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${toString config.services.navidrome.settings.Port}";
+      extraConfig = ''
+        # Set headers
+        proxy_set_header   X-Real-IP $remote_addr;
+        proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header   X-Forwarded-Proto $scheme;
+        proxy_buffering    off;
+      '';
+    };
+  };
+}
