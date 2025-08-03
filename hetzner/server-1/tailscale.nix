5774ec9b210d72798ed0f2869fd5ef8cd9ebe232
Alper Çelik <alper@alper-celik.dev>
2025-08-03 21:00:28 +0300

    dont advertise subnet on hetzner

--- hetzner/server-1/tailscale.nix
+++ hetzner/server-1/tailscale.nix
@@ -5,7 +5,6 @@
     extraSetFlags = [ "--advertise-exit-node" ];
     extraUpFlags = [
       "--advertise-exit-node"
-      "--advertise-routes=192.168.1.0/24"
     ];
     enable = true;
     authKeyFile = config.sops.secrets.tailscale-auth-key-hetzner-server-1.path;
