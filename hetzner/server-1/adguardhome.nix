a9b0235268030d758a09c3aae055ab456173be5b
Alper Çelik <alper@alper-celik.dev>
2025-08-28 22:32:31 +0300

    add unbound recursive resolver

--- hetzner/server-1/adguardhome.nix
+++ hetzner/server-1/adguardhome.nix
@@ -5,6 +5,30 @@
     allowDHCP = false;
   };
 
+  services.unbound = {
+    enable = true;
+
+    settings = {
+      server = {
+        # When only using Unbound as DNS, make sure to replace 127.0.0.1 with your ip address
+        # When using Unbound in combination with pi-hole or Adguard, leave 127.0.0.1, and point Adguard to 127.0.0.1:PORT
+        interface = [ "127.0.0.1" ];
+        port = 5335;
+        access-control = [ "127.0.0.1 allow" ];
+        # Based on recommended settings in https://docs.pi-hole.net/guides/dns/unbound/#configure-unbound
+        harden-glue = true;
+        harden-dnssec-stripped = true;
+        use-caps-for-id = false;
+        prefetch = true;
+        edns-buffer-size = 1232;
+
+        # Custom settings
+        hide-identity = true;
+        hide-version = true;
+      };
+    };
+  };
+
   services.nginx.virtualHosts."adguardhome.lab.alper-celik.dev" = {
     enableACME = true;
     forceSSL = true;
