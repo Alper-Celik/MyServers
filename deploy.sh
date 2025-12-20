de743b5b301dc6e12149e394437feddd0b47a4f1
Alper Çelik <alper@alper-celik.dev>
2025-12-20 15:47:30 +0300

    trust flake config when deploying

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env nix
 #! nix shell .#deploy-rs --command bash
-deploy "$(dirname "$0")" --auto-rollback false --magic-rollback false
+deploy "$(dirname "$0")" --auto-rollback false --magic-rollback false -- --accept-flake-config
