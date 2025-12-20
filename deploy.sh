8f52c899291517136420b6369da2eee4d0c9ccb8
Alper Çelik <alper@alper-celik.dev>
2025-12-20 19:05:27 +0300

    add substituter for deploy/build

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env nix
 #! nix shell .#deploy-rs --command bash
-deploy "$(dirname "$0")" --auto-rollback false --magic-rollback false -- --accept-flake-config
+deploy "$(dirname "$0")" --auto-rollback false --magic-rollback false -- --accept-flake-config --option substituters "https://nixos-raspberrypi.cachix.org" --option trusted-public-keys "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI=" -L
