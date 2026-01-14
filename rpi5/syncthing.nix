27b77c14d99a7732e07076ce6cec8797c10c9eab
Alper Çelik <alper@alper-celik.dev>
2026-01-15 01:34:26 +0300

    add acl for syncthing

--- rpi5/syncthing.nix
+++ rpi5/syncthing.nix
@@ -1,24 +1,57 @@
 { config, ... }:
+let
+  cfg = config.services.syncthing;
+  syncthing-base = "/var/lib/syncthing";
+in
 {
 
   users.users.${config.services.syncthing.user}.home = config.services.syncthing.dataDir;
-  services = {
-    syncthing = {
-      enable = true;
-      settings = {
-        gui.insecureSkipHostcheck = true; # there is already nginx proxy that is behind tailscale
-      };
-      overrideDevices = false;
-      overrideFolders = false;
+  services.syncthing = {
+    enable = true;
+    dataDir = syncthing-base + "/data";
+    configDir = syncthing-base + "/config";
+    settings = {
+      gui.insecureSkipHostcheck = true; # there is already nginx proxy that is behind tailscale
     };
+    overrideDevices = false;
+    overrideFolders = false;
+  };
 
-    nginx.virtualHosts."syncthing-rpi.lab.alper-celik.dev" = {
-      forceSSL = true;
-      enableACME = true;
-      acmeRoot = null;
-      locations."/" = {
-        proxyPass = "http://${config.services.syncthing.guiAddress}";
-      };
+  systemd.tmpfiles.rules =
+    let
+      acl-generate =
+        user-or-group: name: acl:
+        "${user-or-group}:${name}:${acl}";
+      acl-generate-with-defalut =
+        user-or-group: name: acl:
+        let
+          acl-end = acl-generate user-or-group name acl;
+        in
+        "${acl-end},default:${acl-end}";
+      acl-generate-user-and-group =
+        name: acl:
+        let
+          acl-end = user-group: acl-generate-with-defalut user-group name acl;
+        in
+        "${acl-end "user"},${acl-end "group"}";
+      acl = acl-generate-user-and-group;
+    in
+    [
+      "d ${syncthing-base} 0771 ${cfg.user} ${cfg.group} - -" # let acl'ed users/groups pass through by setting executable bit on other
+
+      "d ${cfg.configDir} 0770 ${cfg.user} ${cfg.group} - -"
+      "d ${cfg.dataDir} 0770 ${cfg.user} ${cfg.group} - -"
+
+      "A ${cfg.dataDir} - - - - ${acl "nextcloud" "rwX"}"
+    ];
+
+  services.nginx.virtualHosts."syncthing-rpi.lab.alper-celik.dev" = {
+    forceSSL = true;
+    enableACME = true;
+    acmeRoot = null;
+    locations."/" = {
+      proxyPass = "http://${config.services.syncthing.guiAddress}";
     };
   };
+
 }
