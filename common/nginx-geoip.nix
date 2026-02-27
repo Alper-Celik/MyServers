26467a842b927ab6b5629bb016439890c612c712
Alper Çelik <alper@alper-celik.dev>
2026-02-28 01:37:12 +0300

    nginx logging

--- common/nginx-geoip.nix
+++ common/nginx-geoip.nix
@@ -8,7 +8,7 @@
   options = {
     services.nginx.x-enable-geoip = lib.mkOption {
       type = lib.types.bool;
-      default = true;
+      default = config.services.geoipupdate.enable;
 
       description = ''
         whether to setup geoip2 for nginx
@@ -24,17 +24,29 @@
       appendHttpConfig = ''
         geoip2 /var/lib/GeoIP/GeoLite2-Country.mmdb {
             auto_reload 5m;
+            $geoip2_data_registered_country_code default=XX registered_country iso_code;
+            $geoip2_data_registered_country_name registered_country names en;
             $geoip2_data_country_code default=XX country iso_code;
             $geoip2_data_country_name country names en;
+            $geoip2_data_continent_code continent code;
+            $geoip2_data_continent_name continent names en;
         }
 
         geoip2 /var/lib/GeoIP/GeoLite2-City.mmdb {
             auto_reload 5m;
             $geoip2_data_city_name   city names en;
+            $geoip2_data_subdivision_name  subdivisions 0 names en;
             $geoip2_data_latitude    location latitude;
             $geoip2_data_longitude   location longitude;
             $geoip2_data_accuracy_radius location accuracy_radius;
-        } '';
+        } 
+
+        geoip2 /var/lib/GeoIP/GeoLite2-ASN.mmdb {
+            auto_reload 5m;
+            $geoip2_data_ASN_number  autonomous_system_number;
+            $geoip2_data_ASN_org  autonomous_system_organization;
+        }
+      '';
     };
   };
 }
