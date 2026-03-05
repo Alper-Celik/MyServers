4bd322c1672ee7d85542806c036384200ee5a658
Alper Çelik <alper@alper-celik.dev>
2026-03-06 02:02:28 +0300

    add swap to rpi5
    oom killer sucks

--- rpi5/hardware-configuration.nix
+++ rpi5/hardware-configuration.nix
@@ -81,6 +81,13 @@
     enable = true;
     memoryPercent = 60;
   };
+  swapDevices = [
+    {
+      priority = 0;
+      device = "/.swap-file";
+      size = 12288; # 12 GiB
+    }
+  ];
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
