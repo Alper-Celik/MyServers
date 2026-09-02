1c79c63eaf77c6b8eb5d43f22ef86ae9c4b72677
Alper Çelik <alper@alper-celik.dev>
2026-09-03 01:29:47 +0300

    fix ovhcloud deployment

--- ovhcloud/server-1/secrets.nix
+++ ovhcloud/server-1/secrets.nix
@@ -1,3 +1,16 @@
-{ inputs, ... }: {
+{ inputs, ... }:
+let
+  secrets = inputs.MyServersSecrets;
+in
+{
   imports = [ inputs.sops-nix.nixosModules.sops ];
+  sops = {
+    defaultSopsFile = "${secrets}/secrets/ovhcloud/server-1.yaml";
+    age = {
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      keyFile = "/var/lib/sops-nix/key.txt";
+      generateKey = true;
+    };
+  };
+
 }
