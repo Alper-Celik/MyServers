f4cf62f3ac823c5673da457bb5613dfbe492272f
Alper Çelik <alper@alper-celik.dev>
2026-09-25 00:29:51 +0300

    saving one floating ipv4 address yay

--- hetzner/server-1/riscv-tr-container.nix
+++ hetzner/server-1/riscv-tr-container.nix
@@ -12,16 +12,35 @@
   };
   boot.enableContainers = true;
   virtualisation.containers.enable = true;
+
+  services.caddy.virtualHosts."riscv.alper-celik.dev" = {
+    serverAliases = [ "*.riscv.alper-celik.dev" ];
+    extraConfig = "reverse_proxy https://192.168.100.11";
+  };
+
   containers.riscv-tr = {
     timeoutStartSec = "30m";
     autoStart = true;
     privateNetwork = true;
 
     hostAddress = "192.168.100.1";
-    localAddress = "116.202.186.49";
+    localAddress = "192.168.100.11";
 
     hostAddress6 = "2a01:4f8:c0c:57f0::ff";
     localAddress6 = "2a01:4f8:c0c:57f0::2";
+
+    forwardPorts = [
+      {
+        containerPort = 10000;
+        hostPort = 10000;
+        protocol = "udp";
+      }
+      {
+        containerPort = 4443;
+        hostPort = 4443;
+        protocol = "tcp";
+      }
+    ];
 
     path = inputs.riscv-tr.nixosConfigurations."riscv-tr-1".config.system.build.toplevel;
   };
